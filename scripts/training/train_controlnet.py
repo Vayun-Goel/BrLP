b6f05667b91ae3dfc73d40cd0d7b118f249f3314
Sukhvansh2004 <sukhvanshjain@gmail.com>
2025-05-01 11:36:37 +0530

    Dynamic Collate

--- scripts/training/train_controlnet.py
+++ scripts/training/train_controlnet.py
@@ -1,292 +1,3 @@
-# import os
-# import argparse
-# import warnings
-
-# import numpy as np
-# import pandas as pd
-# import torch
-# import torch.nn.functional as F
-# import nibabel as nib
-# from torch.utils.data import DataLoader
-# from torch.cuda.amp import autocast, GradScaler
-# from torch.utils.tensorboard import SummaryWriter
-# from monai import transforms
-# from monai.data.image_reader import NumpyReader
-# from generative.networks.schedulers import DDPMScheduler
-# from tqdm import tqdm
-# from torch.utils.data._utils.collate import default_collate
-
-# from brlp import const
-# from brlp import utils
-# from brlp import networks
-# from brlp import (
-#     get_dataset_from_pd, 
-#     sample_using_controlnet_and_z
-# )
-
-
-# warnings.filterwarnings("ignore")
-# DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-
-# def concat_covariates(_dict):
-#     """
-#     Provide context for cross-attention layers and concatenate the
-#     covariates in the channel dimension.
-#     """
-#     conditions = [
-#         _dict['followup_age'], 
-#         _dict['sex'], 
-#         _dict['followup_diagnosis'], 
-#         _dict['followup_cerebral_cortex'], 
-#         _dict['followup_hippocampus'], 
-#         _dict['followup_amygdala'], 
-#         _dict['followup_cerebral_white_matter'], 
-#         _dict['followup_lateral_ventricle']        
-#     ]
-#     _dict['context'] = torch.tensor(conditions).unsqueeze(0)
-#     return _dict
-
-
-# def images_to_tensorboard(
-#     writer,
-#     epoch, 
-#     mode, 
-#     autoencoder, 
-#     diffusion, 
-#     controlnet, 
-#     dataset,
-#     scale_factor
-# ):
-#     """
-#     Visualize the generation on tensorboard
-#     """
-#     resample_fn = transforms.Spacing(pixdim=1.5)
-#     random_indices = np.random.choice( range(len(dataset)), 3 ) 
-
-#     for tag_i, i in enumerate(random_indices):
-
-#         starting_z = dataset[i]['starting_latent'] * scale_factor
-#         context    = dataset[i]['context']
-#         starting_a = dataset[i]['starting_age']
-
-#         starting_image = torch.from_numpy(nib.load(dataset[i]['starting_image']).get_fdata()).unsqueeze(0)
-#         followup_image = torch.from_numpy(nib.load(dataset[i]['followup_image']).get_fdata()).unsqueeze(0)
-#         starting_image = resample_fn(starting_image).squeeze(0)
-#         followup_image = resample_fn(followup_image).squeeze(0)
-
-#         predicted_image = sample_using_controlnet_and_z(
-#             autoencoder=autoencoder, 
-#             diffusion=diffusion, 
-#             controlnet=controlnet, 
-#             starting_z=starting_z, 
-#             starting_a=starting_a, 
-#             context=context, 
-#             device=DEVICE,
-#             scale_factor=scale_factor
-#         )
-
-#         utils.tb_display_cond_generation(
-#             writer=writer, 
-#             step=epoch, 
-#             tag=f'{mode}/comparison_{tag_i}',
-#             starting_image=starting_image, 
-#             followup_image=followup_image, 
-#             predicted_image=predicted_image
-#         )
-
-# def custom_collate_fn(batch):
-#     numeric_batch = []
-#     for item in batch:
-#         filtered = {k: v for k, v in item.items() if isinstance(v, (int, float, torch.Tensor))}
-#         numeric_batch.append(filtered)
-#     return default_collate(numeric_batch)
-
-
-# if __name__ == '__main__':
-
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('--dataset_csv', required=True,   type=str)
-#     parser.add_argument('--cache_dir',   required=True,   type=str)
-#     parser.add_argument('--output_dir',  required=True,   type=str)
-#     parser.add_argument('--aekl_ckpt',   required=True,   type=str)
-#     parser.add_argument('--diff_ckpt',   required=True,   type=str)
-#     parser.add_argument('--cnet_ckpt',   default=None,    type=str)
-#     parser.add_argument('--num_workers', default=8,       type=int)
-#     parser.add_argument('--n_epochs',    default=5,       type=int)
-#     parser.add_argument('--batch_size',  default=16,      type=int)
-#     parser.add_argument('--lr',          default=2.5e-5,  type=float)
-    
-#     args = parser.parse_args()
-
-
-#     npz_reader = NumpyReader(npz_keys=['data'])
-#     transforms_fn = transforms.Compose([
-#         transforms.LoadImageD(keys=['starting_latent', 'followup_latent'], reader=npz_reader), 
-#         transforms.EnsureChannelFirstD(keys=['starting_latent', 'followup_latent'], channel_dim=0), 
-#         transforms.DivisiblePadD(keys=['starting_latent', 'followup_latent'], k=4, mode='constant'), 
-#         transforms.Lambda(func=concat_covariates),
-#     ])
-
-#     dataset_df = pd.read_csv(args.dataset_csv)
-#     train_df = dataset_df[ dataset_df.split == 'train' ]
-#     valid_df = dataset_df[ dataset_df.split == 'valid' ]
-#     trainset = get_dataset_from_pd(train_df, transforms_fn, args.cache_dir)
-#     validset = get_dataset_from_pd(valid_df, transforms_fn, args.cache_dir)
-
-#     # print(trainset[0])
-
-#     train_loader = DataLoader(dataset=trainset, 
-#                               num_workers=args.num_workers, 
-#                               batch_size=args.batch_size, 
-#                               shuffle=True, 
-#                               persistent_workers=True, 
-#                               pin_memory=True,
-#                               collate_fn=custom_collate_fn)
-
-#     valid_loader = DataLoader(dataset=validset, 
-#                               num_workers=args.num_workers, 
-#                               batch_size=args.batch_size, 
-#                               shuffle=True, 
-#                               persistent_workers=True, 
-#                               pin_memory=True,
-#                               collate_fn=custom_collate_fn)
-
-
-#     # eorbfow
-
-#     autoencoder = networks.init_autoencoder(args.aekl_ckpt)
-#     diffusion   = networks.init_latent_diffusion(args.diff_ckpt)
-#     controlnet  = networks.init_controlnet()
-
-#     if args.cnet_ckpt is not None:
-#         print('Resuming training...')
-#         controlnet.load_state_dict(torch.load(args.cnet_ckpt))
-#     else:
-#         print('Copying weights from diffusion model')
-#         controlnet.load_state_dict(diffusion.state_dict(), strict=False)
-
-#     # freeze the unet weights
-#     for p in diffusion.parameters():
-#         p.requires_grad = False
-
-#     # Move everything to DEVICE
-#     autoencoder.to(DEVICE)
-#     diffusion.to(DEVICE)
-#     controlnet.to(DEVICE)
-
-#     scaler = GradScaler()
-#     optimizer = torch.optim.AdamW(controlnet.parameters(), lr=args.lr)
-
-#     with torch.no_grad():
-#         with autocast(enabled=True):
-#             z = trainset[0]['followup_latent']
-
-#     scale_factor = 1 / torch.std(z)
-#     print(f"Scaling factor set to {scale_factor}")
-
-#     scheduler = DDPMScheduler(num_train_timesteps=1000, 
-#                               schedule='scaled_linear_beta', 
-#                               beta_start=0.0015, 
-#                               beta_end=0.0205)
-    
-#     writer = SummaryWriter()
-
-#     global_counter  = { 'train': 0, 'valid': 0 }
-#     loaders         = { 'train': train_loader, 'valid': valid_loader }
-#     datasets        = { 'train': trainset, 'valid': validset }
-
-
-#     for epoch in range(args.n_epochs):
-        
-#         for mode in loaders.keys():
-#             print('mode:', mode)
-#             loader = loaders[mode]
-#             controlnet.train() if mode == 'train' else controlnet.eval()
-#             epoch_loss = 0.
-#             progress_bar = tqdm(enumerate(loader), total=len(loader))
-#             progress_bar.set_description(f"Epoch {epoch}")
-
-#             for step, batch in progress_bar:
-#                 # print(batch['num_past_scans'])
-#                 print(batch['context'].shape)
-                
-#                 if mode == 'train':
-#                     optimizer.zero_grad(set_to_none=True)
-
-#                 with torch.set_grad_enabled(mode == 'train'):
-                    
-
-#                     starting_z = batch['starting_latent'].to(DEVICE)  * scale_factor
-#                     followup_z = batch['followup_latent'].to(DEVICE)  * scale_factor
-#                     # starting_z = followup_z.clone()
-#                     context    = batch['context'].to(DEVICE)
-#                     starting_a = batch['starting_age'].to(DEVICE)
-#                     print(f"starting_z.shape: {starting_z.shape}, starting_a.shape: {starting_a.shape}")
-#                     print(f"followup_z.shape: {followup_z.shape}, context.shape: {context.shape}")
-
-#                     n = starting_z.shape[0] 
-
-#                     with autocast(enabled=True):
-
-#                         concatenating_age      = starting_a.view(n, 1, 1, 1, 1).expand(n, 1, *starting_z.shape[-3:])
-#                         controlnet_condition   = torch.cat([ starting_z, concatenating_age ], dim=1)
-
-#                         print(f"controlnet_condition.shape: {controlnet_condition.shape}")
-
-#                         noise = torch.randn_like(followup_z).to(DEVICE)
-#                         timesteps = torch.randint(0, scheduler.num_train_timesteps, (n,), device=DEVICE).long()
-#                         images_noised = scheduler.add_noise(followup_z, noise=noise, timesteps=timesteps)
-
-#                         down_h, mid_h = controlnet(
-#                             x=images_noised.float(), 
-#                             timesteps=timesteps, 
-#                             context=context.float(),
-#                             controlnet_cond=controlnet_condition.float()
-#                         )
-
-#                         noise_pred = diffusion(
-#                             x=images_noised.float(), 
-#                             timesteps=timesteps, 
-#                             context=context.float(), 
-#                             down_block_additional_residuals=down_h,
-#                             mid_block_additional_residual=mid_h
-#                         )
-
-#                         loss = F.mse_loss(noise_pred.float(), noise.float())
-
-#                 if mode == 'train':
-#                     scaler.scale(loss).backward()
-#                     scaler.step(optimizer)
-#                     scaler.update()
-                
-#                 #-------------------------------
-#                 # Iteration end
-#                 writer.add_scalar(f'{mode}/batch-mse', loss.item(), global_counter[mode])
-#                 epoch_loss += loss.item()
-#                 progress_bar.set_postfix({"loss": epoch_loss / (step + 1)})
-#                 global_counter[mode] += 1
-
-#             # Epoch loss
-#             epoch_loss = epoch_loss / len(loader)
-#             writer.add_scalar(f'{mode}/epoch-mse', epoch_loss, epoch)
-            
-#             # Logging visualization
-#             # images_to_tensorboard(
-#             #     writer=writer,
-#             #     epoch=epoch,
-#             #     mode=mode, 
-#             #     autoencoder=autoencoder, 
-#             #     diffusion=diffusion, 
-#             #     controlnet=controlnet,
-#             #     dataset=datasets[mode], 
-#             #     scale_factor=scale_factor
-#             # )
-
-#         if epoch > 2:
-#             savepath = os.path.join(args.output_dir, f'cnet-ep-{epoch}.pth')
-#             torch.save(controlnet.state_dict(), savepath)
-
 import os
 import argparse
 import warnings
@@ -317,7 +28,6 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 def concat_covariates(sample):
-    # unchanged
     conds = [
         sample['followup_age'],
         sample['sex'],
@@ -333,75 +43,51 @@ def concat_covariates(sample):
 
 
 def load_and_stack_multiscans(sample: dict, max_scans: int) -> dict:
-    # 1) Turn follow-up into a pure torch.Tensor (MetaTensor or np.ndarray)
+    # load follow-up latent
     fu = sample['followup_latent_path']
-    if isinstance(fu, np.ndarray):
-        fu_t = torch.from_numpy(fu).float()
-    else:
-        fu_t = torch.as_tensor(fu).float()
+    fu_t = torch.as_tensor(np.load(fu)['data']).float() if isinstance(fu, str) else torch.as_tensor(fu).float()
     sample['followup_latent_path'] = fu_t
+    target_shape = fu_t.shape
 
-    # target shape = (C, H, W, D)
-    target_shape = fu_t.shape  
-    n_past       = int(sample.get('num_past_scans', 0))
-
-    latents = []
-    ages    = []
-
+    latents, ages = [], []
+    n_past = int(sample.get('num_past_scans', 0))
     for i in range(1, max_scans + 1):
-        lp_key  = f'starting{i}_latent_path'
-        age_key = f'starting{i}_age'
-        path    = sample.get(lp_key, None)
-
-        # load or zero-fill
+        lp_key, age_key = f'starting{i}_latent_path', f'starting{i}_age'
+        path = sample.get(lp_key)
         if i <= n_past and isinstance(path, str) and os.path.isfile(path):
-            arr = np.load(path)['data']
-            t   = torch.from_numpy(arr).float()
+            arr = np.load(path)['data']; t = torch.from_numpy(arr).float()
         else:
-            t   = torch.zeros(target_shape, dtype=fu_t.dtype)
-
-        # **pad** t to target_shape if needed
+            t = torch.zeros(target_shape)
+        # pad if needed
         if t.shape != target_shape:
-            c1,h1,w1,d1 = t.shape
-            _,h0,w0,d0 = target_shape
-            # compute how much to pad on each spatial dim
-            dh, dw, dd = h0-h1, w0-w1, d0-d1
-            # pad format: (pad_left_D, pad_right_D,
-            #              pad_left_W, pad_right_W,
-            #              pad_left_H, pad_right_H)
-            pad = (0, max(0, dd),  0, max(0, dw),  0, max(0, dh))
+            diffs = [targ - cur for targ, cur in zip(target_shape, t.shape)]
+            pad = (0, max(diffs[3],0), 0, max(diffs[2],0), 0, max(diffs[1],0))
             t = F.pad(t, pad)
-
         latents.append(t)
-
-        # age or zero
-        age = sample.get(age_key, None)
+        age = sample.get(age_key, 0.0)
         ages.append(float(age) if isinstance(age, (int, float)) else 0.0)
-
-        # remove raw keys
-        sample.pop(lp_key,  None)
-        sample.pop(age_key, None)
-
-    # stack into fixed-length tensors
-    sample['starting_latents'] = torch.stack(latents, dim=0)            # (max_scans, C, H, W, D)
-    sample['starting_ages']    = torch.tensor(ages, dtype=torch.float32)  # (max_scans,)
+        # drop raw keys
+        sample.pop(lp_key, None); sample.pop(age_key, None)
+    sample['starting_latents'] = torch.stack(latents, dim=0)
+    sample['starting_ages'] = torch.tensor(ages)
     return sample
 
-def custom_collate_fn(batch: List[Dict[str, Any]]) -> Dict[str, Any]:
-    # find keys common to every sample
-    common_keys = set(batch[0].keys())
-    for samp in batch[1:]:
-        common_keys &= set(samp.keys())
 
-    collated: Dict[str, Any] = {}
-    for key in common_keys:
-        vals = [samp[key] for samp in batch]
-        first = vals[0]
-        if isinstance(first, (int, float, torch.Tensor)):
-            collated[key] = default_collate(vals)
+def dynamic_collate_fn(batch: List[Dict[str, Any]]) -> Dict[str, Any]:
+    # determine the max scans present in this minibatch
+    max_scans_batch = max(int(s.get('num_past_scans', 0)) for s in batch)
+    # process each sample: stack its scans
+    processed = [load_and_stack_multiscans(s.copy(), max_scans_batch) for s in batch]
+    # then apply default_collate on tensor fields
+    collated = {}
+    # find keys to collate
+    keys = set(processed[0].keys())
+    for k in keys:
+        vals = [s[k] for s in processed]
+        if isinstance(vals[0], torch.Tensor) or isinstance(vals[0], (int, float)):
+            collated[k] = default_collate(vals)
         else:
-            collated[key] = vals  # list of strings or other metadata
-
+            collated[k] = vals
     return collated
 
 # torch.autograd.set_detect_anomaly(True)
@@ -435,7 +121,6 @@ def images_to_tensorboard(writer, epoch, mode,
             starting_image=SI, followup_image=FI, predicted_image=PI
         )
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset_csv', required=True, type=str)
@@ -446,26 +131,18 @@ if __name__ == '__main__':
     parser.add_argument('--cnet_ckpt',   default=None,    type=str)
     parser.add_argument('--num_workers', default=8,       type=int)
     parser.add_argument('--n_epochs',    default=5,       type=int)
-    parser.add_argument('--batch_size',  default=4,       type=int)
+    parser.add_argument('--batch_size',  default=8,       type=int)
     parser.add_argument('--lr',          default=2.5e-5,  type=float)#2.5e-5
     args = parser.parse_args()
 
     df = pd.read_csv(args.dataset_csv)
-    # any column like "starting17_latent_path" → extract the "17"
-    start_cols = [c for c in df.columns if c.startswith('starting') and c.endswith('_latent_path')]
-    MAX_SCANS = max(int(c[len('starting'):c.find('_latent_path')]) for c in start_cols)
-
-    # we only let MONAI load & pad the *follow‐up* latent:
     npz_reader = NumpyReader(npz_keys=['data'])
-    transforms_fn = transforms.Compose([
+    transforms_fn = Compose([
         LoadImageD(keys=['followup_latent_path'], reader=npz_reader),
         EnsureChannelFirstD(keys=['followup_latent_path'], channel_dim=0),
         DivisiblePadD(keys=['followup_latent_path'], k=4, mode='constant'),
-        # pass MAX_SCANS into our lambda
-        Lambda(lambda sample, ms=MAX_SCANS: load_and_stack_multiscans(sample, ms)),
-        Lambda(func=concat_covariates),
+        Lambda(concat_covariates),
     ])
-
 
     train_df = df[df.split == 'train']
     valid_df = df[df.split == 'valid']
@@ -476,12 +153,12 @@ if __name__ == '__main__':
     train_loader = DataLoader(
         trainset, batch_size=args.batch_size, shuffle=True,
         num_workers=args.num_workers, pin_memory=True,
-        persistent_workers=True
+        persistent_workers=True, collate_fn=dynamic_collate_fn
     )
     valid_loader = DataLoader(
         validset, batch_size=args.batch_size, shuffle=False,
         num_workers=args.num_workers, pin_memory=True,
-        persistent_workers=True
+        persistent_workers=True, collate_fn=dynamic_collate_fn
     )
 
     # — model init (unchanged) —
